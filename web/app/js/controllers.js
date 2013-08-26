487276d4ccd6d37a8321f430112e73d7f90a710f
Martin Lemanski <martin.lemanski@gmx.at>
2013-08-26 15:00:54 +0200

    finished account balance input

--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -7,4 +7,39 @@ function BankListCtrl($scope) {
     $scope.bookingitems = [{'bookingdate': 1361401200000, 'accountchange': -2.73, 'bookingtext': 'SPAR DANKT'},
         {'bookingdate': 1361487600000, 'accountchange': 2, 'bookingtext': 'MCDONALDS BLABLABLA'}];
 
+    $scope.pos = 'calculateFromBegin';
+
+    $scope.currentbalance = 0;
+    $scope.startingbalance = 0;
+
+
+    $scope.calculate = function(referenceBalance) {
+        if($scope.pos == 'calculateFromEnd') {
+            $scope.calculateFromEnd(referenceBalance);
+        }
+        if($scope.pos == 'calculateFromBegin') {
+            $scope.calculateFromBegin(referenceBalance);
+        }
+        console.log('wrong pos: ' + $scope.pos);
+    }
+
+    $scope.calculateFromBegin = function(referenceBalance) {
+        var balance = parseFloat(referenceBalance);
+        $scope.startingbalance = balance;
+        for(var i = 0; i < $scope.bookingitems.length; i++) {
+            balance += $scope.bookingitems[i]['accountchange'];
+            $scope.bookingitems[i]['currentbalance'] = balance;
+        }
+        $scope.currentbalance = balance;
+    }
+
+    $scope.calculateFromEnd = function(referenceBalance) {
+        var balance = parseFloat(referenceBalance);
+        $scope.currentbalance = balance;
+        for(var i = $scope.bookingitems.length-1; i >= 0; i--) {
+            $scope.bookingitems[i]['currentbalance'] = balance;
+            balance -= $scope.bookingitems[i]['accountchange'];
+        }
+        $scope.startingbalance = balance;
+    }
 }
--- web/test/unit/controllersSpec.js
+++ web/test/unit/controllersSpec.js
@@ -2,13 +2,40 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
-  describe('BankListCtrl', function() {
-    it('shows a list of account changes', function() {
-        var scope = {};
-        var controller = new BankListCtrl(scope);
-        expect(scope.bookingitems.length).toBe(2);
-        expect(scope.bookingitems[0]['accountchange']).toBe(-2.73);
+describe('controllers', function () {
+    describe('BankListCtrl', function () {
+
+        var scope;
+        var controller;
+        beforeEach(function () {
+            scope = {};
+            controller = new BankListCtrl(scope);
+        })
+
+        it('shows a list of account changes', function () {
+            expect(scope.bookingitems.length).toBe(2);
+            expect(scope.bookingitems[0]['accountchange']).toBe(-2.73);
+
+            expect(scope.currentbalance).toBe(0);
+            expect(scope.startingbalance).toBe(0);
+        });
+
+        it('calculates with reference account balance', function () {
+            scope.calculateFromBegin('1');
+
+            expect(scope.bookingitems[0]['currentbalance']).toBe(-1.73);
+            expect(scope.bookingitems[1]['currentbalance']).toBe(0.27);
+            expect(scope.currentbalance).toBe(0.27);
+            expect(scope.startingbalance).toBe(1);
+        });
+
+        it('calculates with reference account balance', function () {
+            scope.calculateFromEnd('1');
+
+            expect(scope.bookingitems[0]['currentbalance']).toBe(-1);
+            expect(scope.bookingitems[1]['currentbalance']).toBe(1);
+            expect(scope.currentbalance).toBe(1);
+            expect(scope.startingbalance).toBe(1.73);
+        });
     });
-  });
 });

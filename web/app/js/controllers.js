5566c4ced5a2a373bcdcbf0536ce6f6198ed56fc
Martin Lemanski <martin.lemanski@gmx.at>
2013-08-26 07:16:04 +0200

    output on index.html is working

--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -4,7 +4,7 @@
 
 function BankListCtrl($scope) {
 
-    $scope.eintraege = [{'bookingdate': 1361401200000, 'accountchange': -2.73},
-        {'bookingdate': 1361487600000, 'accountchange': 2}]
+    $scope.bookingitems = [{'bookingdate': 1361401200000, 'accountchange': -2.73},
+        {'bookingdate': 1361487600000, 'accountchange': 2}];
 
 }
--- web/test/unit/controllersSpec.js
+++ web/test/unit/controllersSpec.js
@@ -3,5 +3,12 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function() {
-
+  describe('BankListCtrl', function() {
+    it('shows a list of account changes', function() {
+        var scope = {};
+        var controller = new BankListCtrl(scope);
+        expect(scope.bookingitems.length).toBe(2);
+        expect(scope.bookingitems[0]['accountchange']).toBe(-2.73);
+    });
+  });
 });

a4d0893ea47488b9905d597642fe08f1c710f0bb
Martin Lemanski <martin.lemanski@gmx.at>
2013-08-26 22:47:54 +0200

    minor changes: encapsulated parsing, added a third position to account items

--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -5,26 +5,23 @@
 function BankListCtrl($scope) {
 
     $scope.bookingitems = [{'bookingdate': 1361401200000, 'accountchange': -2.73, 'bookingtext': 'SPAR DANKT'},
-        {'bookingdate': 1361487600000, 'accountchange': 2, 'bookingtext': 'MCDONALDS BLABLABLA'}];
+        {'bookingdate': 1361487600000, 'accountchange': 2, 'bookingtext': 'MCDONALDS BLABLABLA'},
+        {'bookingdate': 1361487600000, 'accountchange': 1500, 'bookingtext': 'GEHALT BLABLABLA'}];
 
     $scope.pos = 'calculateFromBegin';
 
     $scope.currentbalance = 0;
     $scope.startingbalance = 0;
 
-
-    $scope.calculate = function(referenceBalance) {
-        if($scope.pos == 'calculateFromEnd') {
-            $scope.calculateFromEnd(referenceBalance);
+    $scope.parseBalance = function(balance) {
+        if(balance == undefined || balance == null) {
+            return 0;
         }
-        if($scope.pos == 'calculateFromBegin') {
-            $scope.calculateFromBegin(referenceBalance);
-        }
-        console.log('wrong pos: ' + $scope.pos);
+        return parseFloat(balance);
     }
 
     $scope.calculateFromBegin = function(referenceBalance) {
-        var balance = parseFloat(referenceBalance);
+        var balance = $scope.parseBalance(referenceBalance);
         $scope.startingbalance = balance;
         for(var i = 0; i < $scope.bookingitems.length; i++) {
             balance += $scope.bookingitems[i]['accountchange'];
@@ -34,7 +31,7 @@ function BankListCtrl($scope) {
     }
 
     $scope.calculateFromEnd = function(referenceBalance) {
-        var balance = parseFloat(referenceBalance);
+        var balance = $scope.parseBalance(referenceBalance);
         $scope.currentbalance = balance;
         for(var i = $scope.bookingitems.length-1; i >= 0; i--) {
             $scope.bookingitems[i]['currentbalance'] = balance;

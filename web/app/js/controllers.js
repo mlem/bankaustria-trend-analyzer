6d9fc61ed5fb1f6b3d36aed9e11ab5ce14d4dc13
Martin Lemanski <martin.lemanski@gmx.at>
2013-08-26 06:52:33 +0200

    implementing solution with angularjs

--- web/app/js/controllers.js
+++ web/app/js/controllers.js
@@ -1,3 +1,10 @@
 'use strict';
 
 /* Controllers */
+
+function BankListCtrl($scope) {
+
+    $scope.eintraege = [{'bookingdate': 1361401200000, 'accountchange': -2.73},
+        {'bookingdate': 1361487600000, 'accountchange': 2}]
+
+}
